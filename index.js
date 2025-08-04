8597bff2367e413860499fe9ceb836cf6ef56e00
Belmonde FIODEKANHOUN <belmondefio@gmail.com>
2025-08-04 17:26:18 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const supabase = require("./supabaseClient");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Publier une pensée
+app.post("/post-thought", async (req, res) => {
+  const { username, avatar_url, content } = req.body;
+
+  if (!content || content.length > 60) {
+    return res.status(400).json({ error: "Pensée trop longue ou vide" });
+  }
+
+  const { data, error } = await supabase.from("thoughts").insert([
+    {
+      username,
+      avatar_url,
+      content,
+      created_at: new Date().toISOString(),
+    },
+  ]);
+
+  if (error) return res.status(500).json({ error });
+
+  res.status(201).json(data[0]);
+});
+
+// Récupérer les pensées valides
+app.get("/thoughts", async (req, res) => {
+  const { data, error } = await supabase
+    .from("thoughts")
+    .select("*")
+    .order("created_at", { ascending: false });
+
+  if (error) return res.status(500).json({ error });
+  res.json(data);
+});
+
+app.listen(PORT, () =>
+  console.log(`🚀 Serveur lancé sur http://localhost:${PORT}`)
+);
+
+// Lancement du nettoyage automatique
+require("./removeExpired");
---
+++ removeExpired.js
@@ -0,0 +1,19 @@
+const cron = require("node-cron");
+const supabase = require("./supabaseClients");
+
+// Toutes les 15min, les pensées de plus de 24h seront supprimé
+
+cron.schedule("*/15 * * * * ", async () => {
+  const oneDayAgo = new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString();
+
+  const { data, error } = await supabase
+    .from("thoughts")
+    .delete()
+    .lt("created_at", oneDayAgo);
+
+  if (error) {
+    console.error("Erreur suppression pensées expirées:", error);
+  } else {
+    console.log(`✅ Pensées expirées supprimées: ${data.length}`);
+  }
+});
